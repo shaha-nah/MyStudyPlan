79fbf3c417f18f3b62f5778f7acf321f78076c3a
shahanah <shahanahbp@gmail.com>
2022-05-22 16:08:21 +0400

    Update task

--- MyStudyPlanUI/my-study-plan/src/Pages/Dashboard/Dashboard.js
+++ MyStudyPlanUI/my-study-plan/src/Pages/Dashboard/Dashboard.js
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 import {variables} from '../../Variables.js';
 import './Dashboard.css';
 import Tag from '../../Component/Tag/Tag';
-import TaskTag from "../../Component/Tag/TaskTag/TaskTag.js";
 
 export default class Dashboard extends Component{
 
@@ -11,7 +10,12 @@ export default class Dashboard extends Component{
     this.state = {
         modules:[],
         chapters:[],
-        tasks:[]
+        tasks:[],
+        TaskId:"",
+        TaskName:"",
+        TaskDueDate:"",
+        TaskStatus:"",
+        ChapterId:""
     }
   }
 
@@ -45,7 +49,7 @@ export default class Dashboard extends Component{
     });
   }
 
-  updateDueDate(taskId, taskDueDate){
+  updateTask(){
     fetch(variables.API_URL+'task',{
       method:'PUT',
       headers:{
@@ -53,8 +57,9 @@ export default class Dashboard extends Component{
         'Content-Type': 'application/json'
       },
       body:JSON.stringify({
-        TaskId:taskId,
-        TaskDueDate:taskDueDate
+        TaskId:this.state.TaskId,
+        TaskDueDate:this.state.TaskDueDate,
+        TaskStatus:this.state.TaskStatus
       })
     })
     .then(res=>res.json())
@@ -63,14 +68,79 @@ export default class Dashboard extends Component{
       this.refreshList();
     }, (error)=>{
       alert(error);
+    });
+
+    this.updateChapter(this.state.TaskName, this.state.TaskStatus, this.state.ChapterId);
+  }
+
+  updateChapter(taskName, taskStatus, chapterId){
+    let chapterStatus;
+
+    if (taskStatus !== "New"){
+        if (taskName === "Revision" && taskStatus === "Completed"){
+            chapterStatus = "Completed";
+        }
+        else if (taskName === "Summary" && taskStatus === "Completed"){
+            chapterStatus = "Ready";
+        }
+        else{
+            chapterStatus = "In Progress"
+        }
+    }
+    else{
+        if (taskName === "Pre-reading"){
+            chapterStatus = "New"
+        }
+    }
+
+    fetch(variables.API_URL+'chapter',{
+        method:'PUT',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            ChapterId:chapterId,
+            ChapterStatus: chapterStatus
+        })
     })
+        .then(res=>res.json())
+        .then((result)=>{
+        alert(result);
+        this.refreshList();
+    }, (error)=>{
+        alert(error);
+    })
+  }
+
+  changeTaskDueDate = (e) => {
+    this.setState({TaskDueDate:e.target.value});
+  }
+
+  changeTaskStatus = (e) => {
+    this.setState({TaskStatus:e.target.value});
+  }
+
+  editClick(task){
+      this.setState({
+        TaskId: task.TaskId,
+        TaskName: task.TaskName,
+        TaskDueDate: task.TaskDueDate,
+        TaskStatus: task.TaskStatus,
+        ChapterId: task.ChapterId
+      });
   }
 
   render(){
     const {
         modules,
         chapters,
-        tasks
+        tasks,
+        TaskId,
+        TaskName,
+        TaskDueDate,
+        TaskStatus,
+        ChapterId
     }=this.state;
 
     return (
@@ -153,24 +223,71 @@ export default class Dashboard extends Component{
                         <div className="box" style={{backgroundColor: color}}>
                             <div className="title">
                                 {task.TaskName}
+                                <button type="button" className="btn float-end" 
+                                    data-bs-toggle="modal" data-bs-target="#taskModal"
+                                    onClick={()=>this.editClick(task)}
+                                    title="Edit"
+                                    >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                    </svg>
+                                </button>
                             </div>
                             <div className="subtitle">
                                 <span><i class="fa-regular fa-clock-four"></i></span>
-                                <div contenteditable="true" onBlur={e => this.updateDueDate(task.TaskId, e.currentTarget.textContent)}>
+                                <div>
                                     {task.TaskDueDate}
                                 </div>
                             </div>
-                            <TaskTag  
-                                TextColor={color} 
-                                TaskId={task.TaskId}
-                                TaskName={task.TaskName}
-                                TaskStatus={task.TaskStatus}
-                                ChapterId={task.ChapterId}
-                            />
+                            <Tag TagText={task.TaskStatus} TextColor={color} />
                         </div>
                     )
                 })}
             </div>
+        </div>
+
+        <div className="modal fade" id="taskModal" tabIndex="-1" aria-hidden="true">
+          <div className="modal-dialog modal-lg modal-dialog-centered">
+            <div className="modal-content bg-dark border-light">
+              <div className="modal-header">
+                <h5 className="modal-title">Update Task</h5>
+                <button type="button" className="btn-close btn-dark" data-bs-dismiss="modal" aria-label="Close">
+                </button>
+              </div>
+
+              <div className="modal-body">
+                <div className="input-group mb-3">
+                  <span className="col-form-label col-sm-2">Task Date</span>
+                  <input type="date" className="form-control bg-dark text-white"
+                    value={TaskDueDate}
+                    onChange={this.changeTaskDueDate}
+                  />
+                </div>
+                <div className="input-group mb-3">
+                  <span className="col-form-label col-sm-2">Task Status</span>
+                  {/* <input type="text" className="form-control bg-dark text-white"
+                    value={TaskStatus}
+                    onChange={this.changeTaskStatus} 
+                  /> */}
+                  <select className="form-control bg-dark text-white"
+                    value={TaskStatus} onChange={this.changeTaskStatus}
+                  >     
+                    <option value=""></option>       
+                    <option value="New">New</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                  </select>
+                </div>
+
+                <button type="button"
+                  className="btn btn-primary float-start"
+                  onClick={()=>this.updateTask()}>
+                    Update
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
     );
